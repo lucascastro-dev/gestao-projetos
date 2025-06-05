08a5d0d596658b4ba5627cdce1473f944aabfbef
lucas.ferreira <lucas.ferreira@mjv.com.br>
2025-06-05 11:04:21 -0300

    Criando projeto no repositorio

---
+++ src/main/java/com/lucastro/gestaoDeProjetos/GestaoDeProjetosApplication.java
@@ -0,0 +1,13 @@
+package com.lucastro.gestaoDeProjetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeProjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeProjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucastro/gestaoDeProjetos/GestaoDeProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucastro.gestaoDeProjetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
